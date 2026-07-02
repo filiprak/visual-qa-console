40ac1776167b357dd264f7511290374f74047ac4
Filip Rak <filip.rak@ikol.com>
2026-07-02 15:46:10 +0200

    Work in progress

--- backend/src/utils/func.ts
+++ backend/src/utils/func.ts
@@ -1,11 +1,11 @@
 export function slugify(input: string): string {
     return input
         .toString()
+        .replace(/[\s_-]+/g, '-') // collapse whitespace/underscores into -
         .normalize('NFKD') // split accented characters
         .replace(/[\u0300-\u036f]/g, '') // remove diacritics
         .toLowerCase()
         .trim()
         .replace(/[^a-z0-9\s-]/g, '') // remove invalid chars
-        .replace(/[\s_-]+/g, '-') // collapse whitespace/underscores into -
         .replace(/^-+|-+$/g, ''); // trim leading/trailing -
 }
