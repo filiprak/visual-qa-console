65169d456f6787e9f4adc7c3ab09720cbb1e78ce
Filip Rak <filiprak@wp.pl>
2026-07-05 11:16:01 +0200

    Work in progress

--- frontend/src/router.ts
+++ frontend/src/router.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Home from './routes/Home.vue';
+import Pipelines from './routes/Pipelines.vue';
 import NotFound from './routes/NotFound.vue';
 import PipelineDetails from './routes/PipelineDetails.vue';
 import Baselines from './routes/Baselines.vue';
@@ -7,7 +7,7 @@ import Baselines from './routes/Baselines.vue';
 export default createRouter({
     history: createWebHistory(),
     routes: [
-        { path: '/', component: Home },
+        { path: '/', component: Pipelines },
         { path: '/baselines', component: Baselines },
         { path: '/pipelines/:id', component: PipelineDetails },
         { path: '/:pathMatch(.*)*', component: NotFound },
--- frontend/src/routes/Pipelines.vue
+++ frontend/src/routes/Pipelines.vue
@@ -10,33 +10,25 @@
                 <div class="basis-[200px]">Tests</div>
                 <div class="basis-[200px]">Created</div>
             </div>
-            <DataPaginated
-                :service="api.pipelines"
-                sort-field="created_at"
-                :sort-order="-1"
-            >
+            <DataPaginated :service="api.pipelines"
+                           sort-field="created_at"
+                           :sort-order="-1">
                 <template #list="{ items }">
                     <div class="mb-3">
-                        <RouterLink
-                            v-for="item in items"
-                            class="flex gap-3 items-center p-4 hover:bg-emphasis hover:text-color-emphasis border-b border-surface"
-                            :to="{ path: `/pipelines/${item.id}` }"
-                            :key="item.id"
-                        >
+                        <RouterLink v-for="item in items"
+                                    class="flex gap-3 items-center p-4 hover:bg-emphasis hover:text-color-emphasis border-b border-surface"
+                                    :to="{ path: `/pipelines/${item.id}` }"
+                                    :key="item.id">
                             <div class="basis-[28px] flex items-center">
-                                <Icon
-                                    v-if="item.details.status == 'passed'"
-                                    name="check-circle"
-                                    size="1.4rem"
-                                    class="text-green-600"
-                                >
+                                <Icon v-if="item.details.status == 'passed'"
+                                      name="check-circle"
+                                      size="1.4rem"
+                                      class="text-green-600">
                                 </Icon>
-                                <Icon
-                                    v-else
-                                    name="times-circle"
-                                    size="1.4rem"
-                                    class="text-red-600"
-                                >
+                                <Icon v-else
+                                      name="times-circle"
+                                      size="1.4rem"
+                                      class="text-red-600">
                                 </Icon>
                             </div>
                             <div class="font-semibold grow-1">#{{ item.id }} {{ item.name }}</div>
@@ -44,31 +36,26 @@
                                 {{ item.branch_name }}
                             </div>
                             <div class="basis-[200px]">
-                                <Tag severity="secondary">
+                                <Tag severity="info">
                                     {{ item.commit_sha }}
                                 </Tag>
                             </div>
                             <div class="flex flex-col justify-start items-start basis-[200px]">
                                 <span>Total: {{ item.details.total }}</span>
-                                <span
-                                    class="text-xs text-yellow-600"
-                                    v-if="item.details.failed"
-                                >
-                                    <Icon name="exclamation-triangle" /> Failed tests: {{ item.details.failed }}
+                                <span class="text-xs text-yellow-600"
+                                      v-if="item.details.failed">
+                                    <Icon name="exclamation-triangle"
+                                          size="0.7rem" /> Failed tests: {{ item.details.failed }}
                                 </span>
-                                <span
-                                    class="text-xs"
-                                    v-else
-                                >
+                                <span class="text-xs"
+                                      v-else>
                                     All passed!
                                 </span>
                             </div>
                             <div class="flex flex-col justify-start items-start basis-[200px]">
                                 <span v-tooltip.top="format(item.created_at)">{{ fromNow(item.created_at) }}</span>
-                                <span
-                                    class="text-xs"
-                                    v-tooltip.top="format(item.updated_at)"
-                                >
+                                <span class="text-xs"
+                                      v-tooltip.top="format(item.updated_at)">
                                     Last updated: {{ fromNow(item.updated_at) }}
                                 </span>
                             </div>
