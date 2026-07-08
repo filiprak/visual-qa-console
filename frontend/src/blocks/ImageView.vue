70d961a961b8f88b5468a2d369616a547266e472
Filip Rak <filiprak@wp.pl>
2026-07-08 17:26:43 +0200

    Work in progress

--- frontend/src/blocks/ImageView.vue
+++ frontend/src/blocks/ImageView.vue
@@ -5,11 +5,25 @@
                 block-scroll
                 position="full">
             <template #header>
-                <div class="flex grow gap-3 justify-center pl-10 text-white">
+                <div class="flex grow gap-3 justify-center pl-[143px] text-white">
                     <div class="text-center">
                         <div class="font-bold text-lg">{{ curr_image?.title }}</div>
                         <div class="text-white/80">{{ curr_image?.width }} x {{ curr_image?.height }} px</div>
                     </div>
+                </div>
+                <div class="pr-9 flex gap-2">
+                    <Button icon="pi pi-search-minus"
+                            text
+                            rounded
+                            @click="zoom('out')"
+                            severity="secondary">
+                    </Button>
+                    <Button icon="pi pi-search-plus"
+                            text
+                            rounded
+                            @click="zoom('in')"
+                            severity="secondary">
+                    </Button>
                 </div>
             </template>
             <div class="h-full">
@@ -70,6 +84,17 @@ function onImgLoad(e: Event, item: ImageView) {
     const img = e.target as HTMLImageElement;
     item.width = img.naturalWidth;
     item.height = img.naturalHeight;
+}
+
+function zoom(dir: 'in' | 'out') {
+    const curr_zoom = sw?.zoom.scale || 1;
+    const speed = 1.2;
+
+    if (dir == 'in') {
+        sw?.zoom.in(curr_zoom * speed);
+    } else {
+        sw?.zoom.in(curr_zoom / speed);
+    }
 }
 
 function onMouseWheel(e: WheelEvent) {
