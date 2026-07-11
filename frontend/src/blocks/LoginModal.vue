efec7908f4ef8b64d049972e2fa7e7af159dc14e
Filip Rak <filiprak@wp.pl>
2026-07-11 16:21:17 +0200

    Work in progress

--- frontend/src/blocks/LoginModal.vue
+++ frontend/src/blocks/LoginModal.vue
@@ -1,41 +1,44 @@
 <template>
-    <Dialog
-        v-model:visible="showLoginModal"
-        modal
-        header="Sign In"
-        :style="{ width: '30rem' }"
-    >
-        <form @submit.prevent="handleLogin" class="flex flex-col gap-4 py-4">
+    <Dialog v-model:visible="showLoginModal"
+            modal
+            :style="{ width: '30rem' }">
+        <template #header>
+            <div class="flex gap-4 items-center">
+                <img src="/logo.svg"
+                     class="size-8">
+                <span class="text-xl font-extrabold">Sign In</span>
+            </div>
+        </template>
+        <form @submit.prevent="handleLogin"
+              class="flex flex-col gap-4 py-4">
             <div class="flex flex-col gap-2">
-                <label for="email" class="font-semibold">Email</label>
-                <InputText
-                    id="email"
-                    v-model="email"
-                    type="email"
-                    required
-                    autofocus
-                    placeholder="Enter your email"
-                />
+                <label for="email"
+                       class="font-semibold">Email</label>
+                <InputText id="email"
+                           v-model="email"
+                           type="email"
+                           required
+                           autofocus
+                           placeholder="Enter your email" />
             </div>
             <div class="flex flex-col gap-2">
-                <label for="password" class="font-semibold">Password</label>
-                <Password
-                    id="password"
-                    v-model="password"
-                    :feedback="false"
-                    toggleMask
-                    required
-                    placeholder="Enter your password"
-                    inputClass="w-full"
-                />
+                <label for="password"
+                       class="font-semibold">Password</label>
+                <Password id="password"
+                          v-model="password"
+                          :feedback="false"
+                          toggleMask
+                          required
+                          placeholder="Enter your password"
+                          inputClass="w-full" />
             </div>
-            
+
             <div class="flex justify-end mt-4">
-                <Button
-                    type="submit"
-                    label="Sign In"
-                    :loading="isLoading"
-                />
+                <Button class="w-full"
+                        type="submit"
+                        size="large"
+                        label="Sign In"
+                        :loading="isLoading" />
             </div>
         </form>
     </Dialog>
