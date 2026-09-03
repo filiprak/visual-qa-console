6f7f41ff740e6df2f7c819c73f76417c484e8c2b
Filip Rak <filiprak@wp.pl>
2026-09-03 02:16:02 +0200

    Work in progress [skip ci]

--- frontend/src/utils/statuses.ts
+++ frontend/src/utils/statuses.ts
@@ -1,4 +1,7 @@
 export const testcaseStatusOpts = [
     { value: 'passed', label: 'Passed' },
     { value: 'failed', label: 'Failed' },
+    { value: 'approved', label: 'Approved' },
+    { value: 'reported', label: 'Reported' },
+    { value: 'new', label: 'New' },
 ];
